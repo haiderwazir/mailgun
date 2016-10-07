fa883132bdd055f3627bdc3d0aaa1d38be9335b9
pudge <haider@isometrylabs.com>
2016-10-08 00:14:33 +0500

    added checks and testing

--- mailer.rb
+++ mailer.rb
@@ -5,38 +5,60 @@ ENV['key']='key-3c2638b7abcb409fc50100a9778eb936'
 ENV['domain']= 'sandboxae24e2ff954d4b43ac75e97a23dbac75.mailgun.org'
 
 	def self.send_email(to)
-		begin
-			mg_client = Mailgun::Client.new(ENV['key'], 'bin.mailgun.net', 'd82eaee5', ssl = false)
-			message_params =  { to:  to, subject: 'StudySoup!', text: 'This is StudySoup message!', campaign_id: "7"}
-			mg_client.send_message ENV['domain'], message_params
-			puts "Message Sent"
-		rescue
-			puts "Unable to send mail! Please try again"
+		if check_email_format(to)
+			begin
+				retries ||= 0
+				"RETRYING! -- try ##{ retries }" if retries > 0 
+
+				mg_client = Mailgun::Client.new(ENV['key'], 'bin.mailgun.net', 'd82eaee5', ssl = false)
+				message_params =  { to:  to, subject: 'StudySoup!', text: 'This is StudySoup message!', campaign_id: "7"}
+				mg_client.send_message ENV['domain'], message_params
+				puts "Message Sent!"
+				return "success"
+			rescue
+				retry if (retries += 1) < 3
+				puts "Unable to send mail! Please try again!"
+			end
+		else
+			puts "Invalid Email Address!"
+			return "incorrect email format"
 		end
 	end
 
 	def self.check_suppressions(email)
-		mg_client = Mailgun::Client.new(ENV['key'])
-		domain = ENV['domain']
-		response = []
-		begin
-			unsubscribes = mg_client.get "#{domain}/unsubscribes/#{email}"
-			response << "UNSUBSCRIBED!"
-		rescue
-			response << "Not unsubscribed"
+		if check_email_format(email)
+			mg_client = Mailgun::Client.new(ENV['key'])
+			domain = ENV['domain']
+			response = []
+			begin
+				unsubscribes = mg_client.get "#{domain}/unsubscribes/#{email}"
+				response << "UNSUBSCRIBED!"
+			rescue
+				response << "Not unsubscribed"
+			end
+			begin
+				complaints = mg_client.get "#{domain}/complaints/#{email}"
+				response = "COMPLAINED!"
+			rescue
+				response << "No complaints"
+			end
+			begin
+				bounces = mg_client.get "#{domain}/bounces/#{email}"
+				response << "BOUNCED!"
+			rescue
+				response << "No bounces"
+			end
+			puts response
+		else
+			puts "Invalid Email Address!"
+			return "incorrect email format"
 		end
-		begin
-			complaints = mg_client.get "#{domain}/complaints/#{email}"
-			response = "COMPLAINED!"
-		rescue
-			response << "No complaints"
-		end
-		begin
-			bounces = mg_client.get "#{domain}/bounces/#{email}"
-			response << "BOUNCED!"
-		rescue
-			response << "No bounces"
-		end
-		puts response
 	end
+
+	private
+
+	def self.check_email_format(email)
+		email.match /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+	end
+
 end
--- test_mailer.rb
+++ test_mailer.rb
@@ -1,25 +1,14 @@
-require 'simplecov'
-require "test/unit"
-
-SimpleCov.start
-
 require_relative "mailer"
-# require 'stringio'
+require "test/unit"
 
 class TestMailer < Test::Unit::TestCase
 
-test 'Mailgun should instantiate an HttpClient object' do
-    expect {@mg_obj = Mailgun::UnitClient.new("messages")}.not_to raise_error
-end
+  test "should raise error if email format is incorrect" do
+    assert Mailer.send_email("wrongemail") == "incorrect email format"
+    assert Mailer.check_suppressions("wrongemail") == "incorrect email format"
+  end
 
-
-
-
-
-
-
-
-
-
-
+  test "should send email format if address is correct" do
+    assert Mailer.send_email("test@test.com") == "success"
+  end
 end
